1a27e5b11f1dd49d71090c669267bc819348db01
Matlhomola Mohlala <tlhoksmatlhomola20@gmail.com>
2026-02-08 17:28:00 +0200

    Add SQL queries for job skills, roles, and locations

---
+++ Script/SQL.sql
@@ -0,0 +1,24 @@
+SELECT
+  skill,
+  COUNT(*) AS demand
+FROM jobs_adzuna_skills
+GROUP BY skill
+ORDER BY demand DESC
+LIMIT 20;
+
+**### Jobs by role
+
+SELECT
+  search_term,
+  COUNT(DISTINCT job_id) AS job_count
+FROM jobs_adzuna
+GROUP BY search_term;
+
+### Jobs by location
+
+SELECT
+  location,
+  COUNT(DISTINCT job_id) AS job_count
+FROM jobs_adzuna
+GROUP BY location
+ORDER BY job_count DESC;
